57354c7b6b3362971301174c3701755885618773
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-24 10:05:11 -0400

    added id

--- read_test/my_hammer.js
+++ read_test/my_hammer.js
@@ -3,7 +3,7 @@ $(function(){
 var box = document.getElementById("black_box")
 console.log(box)
 var hammer = Hammer(box).on("tap", function() {
-       $(#what).text("yolo")
+       $('#what').text("yolo")
     });
 
 });
