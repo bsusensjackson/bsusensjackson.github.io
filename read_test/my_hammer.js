d9568c7e90e41ca125429b79d580d731458534f1
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-24 10:34:06 -0400

    tweaking

--- read_test/my_hammer.js
+++ read_test/my_hammer.js
@@ -1,9 +1,9 @@
 $(function(){
 
-var page = document.getElementById("page")
+var page = document.getElementById("countdown")
 $('#what1').hide()
 var hammer = Hammer(page).on("swiperight", function() {
-       $('#what1').show();
+       // $('#what1').show();
     	$('#what').hide();
     });
 var hammer2 = Hammer(page).on("swiperight", function() {
