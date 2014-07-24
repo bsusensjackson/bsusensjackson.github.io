55ea8ae8b343c2bb49066eb787fd1e2f7f3869b7
Brendan Susens-Jackson <bsusensjackson@gmail.com>
2014-07-24 10:42:26 -0400

    uncommented on swipeleft

--- read_test/my_hammer.js
+++ read_test/my_hammer.js
@@ -3,7 +3,7 @@ $(function(){
 var page = document.getElementById("countdown")
 $('#what1').hide()
 var hammer = Hammer(page).on("swiperight", function() {
-       // $('#what1').show();
+       $('#what1').show();
     	$('#what').hide();
     });
 var hammer2 = Hammer(page).on("swipeleft", function() {
